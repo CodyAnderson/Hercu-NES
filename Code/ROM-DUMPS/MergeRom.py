ee359dff119def44a0a33f59653d550df5cc89ae
Cody Anderson <cody.anderson@digipen.edu>
2017-10-15 13:56:50 -0700

    Fixed merging script

--- Code/ROM-DUMPS/MergeRom.py
+++ Code/ROM-DUMPS/MergeRom.py
@@ -9,7 +9,7 @@ with open('OursHeader.nes', 'wb') as outfile1: #Our ROM
               symbol = infile.read(1)
               outfile1.write(symbol)
               outfile2.write(symbol)
-      with open('MarioProg.bin', 'rb') as infile: 
+      with open('MarioChar.bin', 'rb') as infile: 
           for each in range(8192): #Hardcoded CHAR ROM size
               symbol = infile.read(1)
               outfile1.write(symbol)
