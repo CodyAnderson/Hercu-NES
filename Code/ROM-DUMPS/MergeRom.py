27dfba43f0b4b27f4c500d88bae24eb83f4f955b
Cody Anderson <cody.anderson@digipen.edu>
2017-10-15 13:45:30 -0700

    ROM Dump folder
    Added a rom dump folder along with a script to merge iNES header, PROG
    ROM and CHAR ROM into an emulation read ROM
    
    Correct header and ROM dumps still needed

---
+++ Code/ROM-DUMPS/MergeRom.py
@@ -0,0 +1,17 @@
+with open('OursHeader.nes', 'wb') as outfile1: #Our ROM
+    with open('Ours.nes', 'wb') as outfile2: #Our ROM no header
+      with open('MarioHeader.bin', 'rb') as infile: 
+          for each in range(16): #Hardcoded header size
+              symbol = infile.read(1)
+              outfile1.write(symbol)
+      with open('MarioProg.bin', 'rb') as infile: 
+          for each in range(32768): #Hardcoded PROG ROM size
+              symbol = infile.read(1)
+              outfile1.write(symbol)
+              outfile2.write(symbol)
+      with open('MarioProg.bin', 'rb') as infile: 
+          for each in range(8192): #Hardcoded CHAR ROM size
+              symbol = infile.read(1)
+              outfile1.write(symbol)
+              outfile2.write(symbol)
+            
