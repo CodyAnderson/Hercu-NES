62f6a2496537592f343c941605253a0baefbf8b7
Megzuki <megzuki@gmail.com>
2017-11-19 15:49:18 -0800

    micromerge cpu project

--- Code/FPGA-Nercu-NES/NoES.sv
+++ Code/FPGA-Nercu-NES/NoES.sv
@@ -21,39 +21,99 @@ module NoES(
 	
 	input [9:0]SW,
 	output [7:0]LEDG,
+	output [9:0]LEDR,
 	output CON1_D1
 	
 	);
 	
+<<<<<<< HEAD
 	assign PPU_RD = 0;
 	assign ROMSEL = 0;
-	assign LEDG = CPU_D;
+	assign LEDG = PPU_D;
+	assign PPU_WR = 1;
 	
-	
-	logic [15:0]address = 0;
-	assign CPU_A = {5'b0,SW};//address;
+	logic [14:0]address = 0;
+	assign PPU_A = address;//address;
 	logic newData = 0;
 	logic [7:0]data = 0;
 	logic done;
+=======
+	//assign PPU_RD = 0;
+	//assign ROMSEL = 0;
 	
-	UartTransmit uart(CLOCK_50, newData, data, CON1_D1, done);
+	
+	logic [15:0] addressOut = 0;
+	//assign CPU_A = {5'b0,SW};//address;
+	//logic newData = 0;
+	logic [7:0] data;
+	//logic done;
+	
+	
+	logic [26:0]clockDivider = 0;
+	logic clock = 1;
+	
+	assign LEDG = data;
+	
+	assign LEDR[9] = clock;
+	assign LEDR[8:3] = 6'b000000;
+	assign LEDR[2:0] = addressOut[2:0];
+	
+	//UartTransmit uart(CLOCK_50, newData, data, CON1_D1, done);
+	NesCpu(clock, {1'b0}, IRQ, {1'b0}, data, addressOut, , , , , , );
+>>>>>>> refs/remotes/origin/Cody
+	
 	always_ff@(posedge CLOCK_50)
 	begin
-		if(done && SW[0] && !address[15] && !newData)
+<<<<<<< HEAD
+		if(done && SW[0] && !address[14] && !newData)
 		begin
 			newData <= 1;
-			data <= CPU_D;
+			data <= PPU_D;
 			address <= address + 1;
-		end
-		else
+=======
+		clockDivider <= clockDivider + 1;
+		clock <= clockDivider[26];
+	end
+	
+	always_ff@(negedge clock)
+	begin
+		if(addressOut[2:0] == 0)
 		begin
-			newData <= 0;
+			data <= 8'hA9;
 		end
-		if(!SW[0])
+		else if (addressOut[2:0] == 1)
 		begin
-			address <= 0;
+			data <= 8'h00;
 		end
-		
+		else if (addressOut[2:0] == 2)
+		begin
+			data <= 8'hF0;
+		end
+		else if (addressOut[2:0] == 3)
+		begin
+			data <= 8'h00;
+		end
+		else if(addressOut[2:0] == 4)
+		begin
+			data <= 8'hF0;
+		end
+		else if (addressOut[2:0] == 5)
+		begin
+<<<<<<< HEAD
+			data <= 8'h00;
+>>>>>>> refs/remotes/origin/Cody
+=======
+			data <= 8'hFA;
+>>>>>>> refs/remotes/origin/Ryan
+		end
+		else if (addressOut[2:0] == 6)
+		begin
+			data <= 8'hFF;
+		end
+		else if (addressOut[2:0] == 7)
+		begin
+			data <= 8'hFF;
+		end
 	end
 	
 endmodule
--- Code/FPGA-Nercu-NES/db/NoES.constraints.sdc
+++ Code/FPGA-Nercu-NES/db/NoES.constraints.sdc
@@ -4,7 +4,15 @@
 #  VENDOR: Altera
 # PROGRAM: Quartus II
 # VERSION: Version 13.0.0 Build 156 04/24/2013 SJ Web Edition
-#    DATE: Sat Oct 14 19:56:47 2017
+<<<<<<< HEAD
+<<<<<<< HEAD
+#    DATE: Sun Oct 15 13:08:44 2017
+=======
+#    DATE: Mon Oct 16 19:46:10 2017
+>>>>>>> refs/remotes/origin/Cody
+=======
+#    DATE: Wed Oct 25 18:03:06 2017
+>>>>>>> refs/remotes/origin/Ryan
 #
 ########################################################################
 
@@ -13,7 +21,8 @@
 # Create Clock
 #**************************************************************
 
-create_clock -name CLOCK_50 -period 1.000 -waveform { 0.000 0.500 } $_col0
+create_clock -name clock -period 1.000 -waveform { 0.000 0.500 } $_col4
+create_clock -name CLOCK_50 -period 1.000 -waveform { 0.000 0.500 } $_col3
 
 
 #**************************************************************
